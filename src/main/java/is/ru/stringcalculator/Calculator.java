bc42fc09b78c80cfc405e0dad9f5c871625f22ef
Skúli Þór <skulij11@ru.is>
2016-10-13 17:35:15 +0000

    Refactoring: splitNumbers and splitWithAnotherDelimiter joined together and some tidying up

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -2,22 +2,15 @@ package is.ru.stringcalculator;
 
 public class Calculator {
 	
-	private static String delimiter = "\n|,";
-
 	public static int add(String text) {
 		if(text.equals(""))
 			return 0;
-		else if(text.startsWith("//")) {
-			return total(splitWithAnotherDelimiter(text));
-		}
 		else if(text.contains("-")) {
 			String exceptionMessage = makeExceptionMessage(text);
 			throw new IllegalArgumentException(exceptionMessage);
-		}
-		
-		else {
+		}	
+		else 
 			return total(splitNumbers(text));
-		}		
     }
 
 	private static String makeExceptionMessage(String text) {
@@ -41,18 +34,21 @@ public class Calculator {
 		return Integer.parseInt(text);
 	}
 	
-	private static String[] splitWithAnotherDelimiter(String text) {
-		String[] splitString = text.substring(2).split("\n", 2);
-		String newDelimiter = splitString[0];
+	private static String[] splitNumbers(String text) {
+		String delimiter = "\n|,";
+		String[] numbers;
 
-		String[] numbers = splitString[1].split(newDelimiter + "|" + delimiter);
+		if(text.startsWith("//")) {
+			String[] splitString = text.substring(2).split("\n", 2);
+			String newDelimiter = splitString[0];
+			numbers = splitString[1].split(newDelimiter + "|" + delimiter);
+		}
+		else 
+			numbers = text.split(delimiter);
+		
 		return numbers;
 	}
 
-	private static String[] splitNumbers(String text) {
-		return text.split(delimiter);
-	}
-	
 	private static int total(String[] numbers) {
 		int total = 0;
 		for(String num : numbers) {
